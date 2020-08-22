43a3cad33da017357140db13e602214cced2844a
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-08-22 17:10:12 -0500

    pokemon are appearing on cards and are clickable to show detail

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'package:pokemon_flutter/pokemon.dart';
+import 'package:pokemon_flutter/pokemondetail.dart';
 
 void main() => runApp(MaterialApp(
       title: 'Poke App',
@@ -9,12 +13,14 @@ void main() => runApp(MaterialApp(
 
 class HomePage extends StatefulWidget {
   @override
-  _HomePageState createState() => _HomePageState();
+  HomePageState createState() => HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class HomePageState extends State<HomePage> {
   var url =
       'https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json';
+
+  PokeHub pokeHub;
 
   @override
   void initState() {
@@ -24,20 +30,71 @@ class _HomePageState extends State<HomePage> {
     fetchData();
   }
 
-  fetchData() async {}
+  fetchData() async {
+    var res = await http.get(url);
+    var decodedJSON = jsonDecode(res.body);
+    pokeHub = PokeHub.fromJson(decodedJSON);
+    print(pokeHub.toJson());
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Poke App'),
-        backgroundColor: Colors.cyan,
+        title: Text('Yasmin\'s PokèApp'),
+        backgroundColor: Colors.red,
       ),
-      body: Center(child: Text('Hello From Pokemon App')),
+      body: pokeHub == null
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : GridView.count(
+              crossAxisCount: 2,
+              children: pokeHub.pokemon
+                  .map((poke) => Padding(
+                        padding: const EdgeInsets.all(2.0),
+                        child: InkWell(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => PokeDetail(
+                                          pokemon: poke,
+                                        )));
+                          },
+                          child: Hero(
+                            tag: poke.img,
+                            child: Card(
+                              elevation: 3.0,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                children: <Widget>[
+                                  Container(
+                                    height: 100.0,
+                                    width: 100.0,
+                                    decoration: BoxDecoration(
+                                        image: DecorationImage(
+                                            image: NetworkImage(poke.img))),
+                                  ),
+                                  Text(
+                                    poke.name,
+                                    style: TextStyle(
+                                        fontSize: 20.0,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ))
+                  .toList(),
+            ),
       drawer: Drawer(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
-        backgroundColor: Colors.cyan,
+        backgroundColor: Colors.red,
         child: Icon(Icons.refresh),
       ),
     );
