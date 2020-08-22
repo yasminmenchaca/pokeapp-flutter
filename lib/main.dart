9a23c1fe74f5ec8401114af0ea642b00510994eb
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-08-22 14:24:37 -0500

    in progress

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() => runApp(MaterialApp(
       title: 'Poke App',
       home: HomePage(),
       debugShowCheckedModeBanner: false,
     ));
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var url =
+      'https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json';
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    fetchData();
+  }
+
+  fetchData() async {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Poke App'),
+        backgroundColor: Colors.cyan,
+      ),
+      body: Center(child: Text('Hello From Pokemon App')),
+      drawer: Drawer(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.cyan,
+        child: Icon(Icons.refresh),
+      ),
+    );
+  }
+}
